f8e25f18781d57f9ae95b84ee9b4d56e78aa38ca
Heinriches <heydrich.institut@gmail.com>
2026-04-23 15:19:54 -0300

    Create Exercicio_005.py

---
+++ Exercicio_005.py
@@ -0,0 +1,9 @@
+print("Nota 1: ")
+nota1 = float(input())
+
+print("Nota 2: ")
+nota2 = float(input())
+
+media = ((nota1 + nota2) / 2)
+
+print(f"A média entre {nota1} e {nota2} é igual a {media}.")
