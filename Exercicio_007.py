ed2bae10e755b51be3fc50d8b5bcb92f43d0b180
Heinriches <heydrich.institut@gmail.com>
2026-04-24 14:25:12 -0300

    Add files via upload

---
+++ Exercicio_007.py
@@ -0,0 +1,7 @@
+number = float(input("Digite um número: "))
+
+twice_as_much = (number * 2)
+third_part = (number / 3)
+
+print(f"O dobro de {number} é {twice_as_much}.")
+print(f"A terça parte de {number} é {third_part:.2f}.")
